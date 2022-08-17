bb54a789ba89bd5c7e48cfe840128c154faedc82
luizaugusto <luizaugustom030502@gmail.com>
2022-08-17 14:55:23 -0300

    Aplicando o Morgan

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import morgan from "morgan";
 
 class App {
     constructor() {
@@ -12,6 +13,7 @@ class App {
     middlewares() {
         this.server.use(express.json());
         this.server.use(cors());
+        this.server.use(morgan("dev"));
     }
 
     routes() {
