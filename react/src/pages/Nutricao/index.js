11d953e61ca6320417de36e6577dd75ec8866d68
luizaugusto <luizaugustom030502@gmail.com>
2022-09-12 18:16:14 -0300

    Front nutricao

--- react/src/pages/Nutricao/index.js
+++ react/src/pages/Nutricao/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import './styles.css';
 import Image from '../../img/logout.png';
+import 
+{ 
+    DialogActions, 
+    DialogContent,
+    TextField, 
+    Alert, 
+    AlertTitle, 
+    Snackbar,
+    Select,
+    MenuItem,
+    InputLabel
+} from '@mui/material';
 
 const Nutricao = () => {
    
@@ -14,7 +26,7 @@ const Nutricao = () => {
                 <button className='ToHome' onClick={  () => navigate("/")}><img src={Image}  className="Home-button" alt="" width={40} height={40}/></button>
             </div>
             <div class="on-screen">
-            
+                //<TextField  style={width: 50%'} id="search" placeholder="Animal" variant="outlined" margin="dense" label="Animal" fullWidth />
             </div>
         </div>
     );
